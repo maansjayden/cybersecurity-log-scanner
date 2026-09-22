9b9da282a7189f73ed387e926fd9248949045423
maansjayden <jamaacpt025@student.wethinkcode.co.za>
2026-09-22 21:50:33 +0200

    why is this returning null

--- scanner.py
+++ scanner.py
@@ -2,9 +2,11 @@ import re
 
 LOG_FILE = "server.log"
 sqli_pattern = re.compile(r"UNION\s+SELECT", re.IGNORECASE)
+ip_pattern = re.compile(r"^(\d+\.\d+\.\d+\.\d+)")
 
 with open(LOG_FILE, "r") as f:
     for line in f:
         line = line.strip()
-        if sqli_pattern.search(line):
-            print("SQLi detected:", line)
+        # regex search returning none on some lines
+        ip = ip_pattern.search(line).group(1)
+        print("ip:", ip)
