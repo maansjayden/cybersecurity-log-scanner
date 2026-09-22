367fe48060c3eabfd85cf60341bbe15b4cb5e2ad
maansjayden <jamaacpt025@student.wethinkcode.co.za>
2026-09-22 17:10:18 +0200

    added regex for sqli

--- scanner.py
+++ scanner.py
@@ -1,5 +1,10 @@
+import re
+
 LOG_FILE = "server.log"
+sqli_pattern = re.compile(r"UNION\s+SELECT", re.IGNORECASE)
 
 with open(LOG_FILE, "r") as f:
     for line in f:
-        print(line.strip())
+        line = line.strip()
+        if sqli_pattern.search(line):
+            print("SQLi detected:", line)
